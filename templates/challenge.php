05da7175b16397f21cf888553ea2ab054d39217f
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-08-27 12:41:57 +0200

    use app svg

--- templates/challenge.php
+++ templates/challenge.php
@@ -12,6 +12,6 @@ style('twofactor_u2f', 'style');
 	<input id="challenge" type="hidden" name="challenge">
 </form>
 
-<img src="<?php print_unescaped(image_path('twofactor_u2f', 'u2f.svg')); ?>">
+<img src="<?php print_unescaped(image_path('twofactor_u2f', 'app.svg')); ?>">
 <p><?php p($l->t('Please plug in your U2F device and press the device button to authorize.')) ?></p>
 <p><em><?php p($l->t('Chrome is the only browser that supports U2F devices. You need to install the "U2F Support Add-on" on Firefox to use U2F.')) ?></em></p>
