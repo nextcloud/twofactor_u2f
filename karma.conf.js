c91a297fa56a9483c039e8eb888b687fdc68a01a
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-07 13:51:40 +0100

    Try to exclude test files from test coverage
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'js/**[!vendor]/*[!spec].js': ['coverage']
+			'js/*.js': ['coverage']
 		},
 
 		// test results reporter to use
