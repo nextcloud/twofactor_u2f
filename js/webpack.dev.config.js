9599393a39f8311cb6fc79ec140a6f04935810a5
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-08-09 09:34:24 +0200

    Fix webpack build
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- js/webpack.dev.config.js
+++ js/webpack.dev.config.js
@@ -2,5 +2,6 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config.js');
 
 module.exports = merge(baseConfig, {
-	devtool: 'inline-source-map'
+	devtool: 'inline-source-map',
+	mode: 'development'
 });
